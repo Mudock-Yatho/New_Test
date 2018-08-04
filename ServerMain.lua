defa5a25b9ddc2136c0a5fc94a4071f6d0a975f4
Mudock R Yatho <MudockYatho@Gmail.com>
2018-08-04 13:43:48 -0500

    Update ServerMain.lua

--- ServerMain.lua
+++ ServerMain.lua
@@ -1,6 +1,6 @@
 --[[
 	The server's main script that handles stats and remotes.
-	Last updated: 8/3/2018
+	Last updated: 8/4/2018
 	
 	Quite a few things are localized here that you may notice aren't used, this is because I've removed **1** part of the
 	script that isn't open-sourced.
